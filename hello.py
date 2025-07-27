6cb9031b1b835941b273f2fdffd9fcc6eb7022ca
adarshaadi06 <adarshn54688@gmail.com>
2025-07-27 13:42:07 +0530

    sumof2 and hello programs are added

---
+++ hello.py
@@ -0,0 +1,4 @@
+user = input('enter the name to greet: ')
+
+print("Hello " +user+ "!! nice to meet you")
+print(f'hello {user}')
---
+++ sumof2.py
@@ -0,0 +1,8 @@
+def sum(x, y):
+    return x + y
+
+
+x = int(input('enter x: '))
+y = int(input('enter y: '))
+
+print(sum(x, y))
